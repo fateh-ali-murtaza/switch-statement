529628d51f6154aaf102c39dbb86cd63a32d77e3
Muhammad Fateh Ali Murtaza <125380303+fateh-ali-murtaza@users.noreply.github.com>
2023-02-21 19:26:26 +0500

    Add files via upload

---
+++ Switch statement.rb
@@ -0,0 +1,12 @@
+#case statement
+age =87
+case age
+when 0...5
+    puts "Baby"
+    when 5..10
+        puts "Liitle Boy"
+        when 10..15
+            "Young"
+        else 
+            puts " Undefined"
+        end
